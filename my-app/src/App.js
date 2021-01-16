63a3a59fd3cab5eaaa1e7e60a69375ede226232e
Sidharth Khabiya <skhabiya@ucsc.edu>
2021-01-16 11:24:30 -0800

    added sidebar css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import 'react-pro-sidebar/dist/css/styles.css';
 function App() {
   return (
-    <ProSidebar>
-      <Menu iconShape="square">
-        <MenuItem>Dashboard</MenuItem>
-        <SubMenu title="Components">
-          <MenuItem>Component 1</MenuItem>
-          <MenuItem>Component 2</MenuItem>
-        </SubMenu>
-      </Menu>
-    </ProSidebar>
+    <div class="pro-sidebar-layout">
+      <ProSidebar>
+        <Menu iconShape="square">
+          <MenuItem>Dashboard</MenuItem>
+          <MenuItem>Dashboard</MenuItem>
+          <MenuItem>Dashboard</MenuItem>
+          <SubMenu title="Components">
+            <MenuItem>Component 1</MenuItem>
+            <MenuItem>Component 2</MenuItem>
+            <MenuItem>Component 2</MenuItem>
+          </SubMenu>
+        </Menu>
+      </ProSidebar>
+    </div>
   );
 }
 
